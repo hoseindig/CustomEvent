1412d8e447271395e7923e9e037bd4e47bd2a165
hoseinx <hoseinx@gmail.com>
2025-08-02 09:20:04 +0330

    edit

---
+++ app-b/app-a/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>App-b</h1>
+    <p>Waiting for event...</p>
+    <p v-if="message">Received: {{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "" };
+  },
+  mounted() {
+    window.parent.addEventListener("my-event", (e) => {
+      this.message = e.detail?.message || "No message";
+    });
+  },
+};
+</script>
